833081107be6679f1eb7986b0c536dec8e4f8349
matt-long <mclong@ucar.edu>
2022-05-09 10:59:51 -0600

    initial dev

--- onets_lab/__init__.py
+++ onets_lab/__init__.py
@@ -4,3 +4,6 @@ try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:  # pragma: no cover
     __version__ = '0.0.0'  # pragma: no cover
+
+from .box_models import *
+from .glodap import open_glodap
